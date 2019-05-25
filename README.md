2fc6ae016ec98d8dcbb98d02e952811e911f468d
Xetera <mythologyali@gmail.com>
2019-05-24 23:14:10 -0700

    first working prototype

---
+++ README.md
@@ -0,0 +1,24 @@
+# Reddit NSFW Webhook Filter
+
+Tired of your IFTTT Reddit webhook sending lewd content in your server?
+
+### Steps for setting up the webhook server
+1. None, it's all set up for you already! Target https://webhook.hifumi.io with your webhook requests.
+
+### Configuring with IFTTT
+1. Set your target url to https://webhook.hifumi.io
+2. Set your method to POST
+3. Content type should be `application/json`
+4. Fill in the body
+```json
+{
+  "to": "your-actual-webhook-url-here",
+  "url": "{{PostURL}}"
+}
+```
+
+Your final screen should look like this
+
+![](https://i.love.miki.ai/%F0%9F%98%8B%F0%9F%A4%B2%F0%9F%91%AA%E2%9C%8D%F0%9F%91%84%F0%9F%98%9A.png)
+
+**Note:** This currently only supports discord embed format. Customizable post body will be added later.
--- handler.ts
+++ handler.ts
@@ -2,41 +2,87 @@ import { APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 import axios from "axios";
 import "source-map-support/register";
 
-const cleanUrlQueries = (url: string): string => {
-  const index = url.indexOf("?");
-  if (index === -1) {
-    return url;
-  }
-  return url.slice(0, index);
-};
+const cleanUrlQueries = (url: string): string => url.replace(/\/?\?.*/, "");
 
 const makeBody = (str: object) => JSON.stringify(str, null, 2);
 
-const lewd: APIGatewayProxyResult = {
-  statusCode: 403,
-  body: makeBody({
-    reason: `Lewd!`
-  })
-};
+const oops = (statusCode: number, reason: string): APIGatewayProxyResult => ({
+  statusCode,
+  body: makeBody({ reason })
+});
 
-export const forwardWebhook: APIGatewayProxyHandler = async (
-  { body },
-) => {
-  const { to, url } = JSON.parse(body);
-  const cleanUrl = cleanUrlQueries(url);
+const lewd = oops(403, `Lewd!`);
+const is400 = (reason: string) => oops(400, reason);
 
-  const metadataUrl = `${cleanUrl}/about.json`;
-  const meta = await axios.get(metadataUrl).then(r => r.data);
+export const noGet: APIGatewayProxyHandler = () => Promise.resolve({
+  statusCode: 200,
+  body: `You're in the right place! Make sure to do a POST request and not GET, more info at https://github.com/Xetera/reddit-nsfw-webhook-filter.`
+})
 
-  if (meta.over_18) {
-    return lewd;
+export const forwardWebhook: APIGatewayProxyHandler = async ({ body }) => {
+  try {
+    // TODO: allow users to override the body
+    console.log(body);
+
+    const parsed = JSON.parse(body);
+
+    if (!parsed) {
+      return is400(
+        `Invalid post body, https://github.com/Xetera/reddit-nsfw-webhook-filter for more info.`
+      );
+    }
+
+    const { to, url } = parsed;
+
+    if (!to || !url) {
+      return is400(
+        `Post body must include 'to' and 'url' fields, https://github.com/Xetera/reddit-nsfw-webhook-filter for more info.`
+      );
+    }
+
+    const cleanUrl = cleanUrlQueries(url);
+
+    const metadataUrl = `${cleanUrl}/about.json`;
+
+    const meta = await axios.get(metadataUrl).then(r => r.data);
+    const { data } = meta[0].data.children[0];
+
+    if (data.over_18) {
+      return lewd;
+    }
+
+    try {
+      const payload = JSON.stringify({
+        embeds: [
+          {
+            title: data.title,
+            url: `https://reddit.com${data.permalink}`,
+            description: `@${data.author}`,
+            image: {
+              url: data.url
+            }
+          }
+        ]
+      });
+
+      await axios.post(to, payload, {
+        headers: {
+          'Content-Type': "application/json"
+        }
+      });
+
+      return {
+        statusCode: 200,
+        body: makeBody({
+          description: `Posted new image by ${data.author}`
+        })
+      };
+    } catch (err) {
+      console.error(err)
+      return oops(500, err);
+    }
+  } catch (error) {
+    console.error(error)
+    return oops(500, error);
   }
-
-  await axios.post(to, {});
-  return {
-    statusCode: 200,
-    body: makeBody({
-      reason: `Testing`
-    })
-  };
 };
